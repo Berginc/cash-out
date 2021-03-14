5cd003d20a0205cb99062354dd915e2b2625d69a
Berginc <78826783+Berginc@users.noreply.github.com>
2021-03-14 14:02:12 +0000

    Add files via upload

--- cash-out.js
+++ cash-out.js
@@ -4,23 +4,11 @@
 // Cash Out Function
 function cashOutValue(currentOdds, originalOdds, stake) {
   const cashOut = (originalOdds * stake) / currentOdds;
-  const cashOutSummary = `Your cash out is €${cashOut}, from your stake of €${stake}. If you lay €${cashOut} on a Betting Exchange, you'll lock in €${
-    cashOut - stake
-  } profit. Note: You'll need a balance of €${
-    currentOdds * cashOut
-  } to lay this bet`;
+  const hedge = cashOut - stake;
+  const liability = currentOdds * cashOut;
+  const cashOutSummary = `Your cash out is €${cashOut}, from your stake of €${stake}. If you lay €${cashOut} on a Betting Exchange, you'll lock in €${hedge} profit. Note: You'll need a balance of €${liability} to lay this bet`;
   return cashOutSummary;
 }
-
-// console.log(HedgingCalculator(10, 20, 30));
-
-//Hedge
-// function hedgeAmount(cashOut, stake) {
-//   const hedge = cashOut - stake;
-//   return hedge;
-// }
-
-// console.log(hedgeAmount(10, 109));
 
 // Input
 let btn = document.getElementById("btn");
